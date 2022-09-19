8952b1c7f47e17a8643561269db723e7ad2bab68
kevinmerino <kevin.merino.ga@gmail.com>
2022-09-19 01:43:53 -0500

    recien creado login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { VistaComponent } from './vista/vista.component';
+import { SingUPComponent } from './sing-up/sing-up.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    VistaComponent
+    VistaComponent,
+    SingUPComponent
   ],
   imports: [
     BrowserModule,
